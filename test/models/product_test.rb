bf920ffeeb936fc0feca1ba7e76373c1a5cbbb19
Austin Miles <abm@austinbmiles.com>
2017-05-16 23:41:01 -0400

    added validation to products model and controller

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+
+  test "product price must be positive" do
+    product = Product.new(title: 'My Book',
+                          description: 'yyyyy',
+                          image_url: 'zzzz.png')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
+
+  def new_product(image_url)
+    Product.new(title: 'My Book',
+                description: 'yyyyy',
+                image_url: image_url,
+                price: 1)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid? "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid? "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:one).title,
+                          description: 'Blah',
+                          image_url: 'yyy.png',
+                          price: 1)
+
+    assert product.invalid?
+    assert ["has already been taken"], product.errors[:title]
+  end
+
+
+
 end
