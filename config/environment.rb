2a72aefc5ba0149fe346ecc9bd274e7d09775e2a
Austin Miles <abm@austinbmiles.com>
2017-07-31 12:44:11 -0400

    extracted smtp settings to env vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
+# pull in SMTP reference
+require Rails.root.join('config/smtp')
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
 
-Depot::Application.configure do
+Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.action_mailer.default_url_options = { host: "localhost:3000" }
