29f2893b56c0e6e2bed14b74fa93e006d439110b
Austin Miles <abm@austinbmiles.com>
2017-08-15 12:48:33 -0400

    async email working with integration tests

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,3 @@ require Rails.root.join('config/smtp')
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
-end
--- config/smtp.rb
+++ config/smtp.rb
@@ -3,10 +3,8 @@ SMTP_SETTINGS = {
     authentication: :plain,
     domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
     enable_starttls_auto: true,
-    #password: ENV.fetch("SMTP_PASSWORD"),
-    password: ENV.fetch("GMAIL_PASSWORD"),
-    #port: "587",
-    port: 1025,
-    #user_name: ENV.fetch("SMTP_USERNAME")
-    user_name: ENV.fetch("GMAIL_USERNAME")
+    password: ENV.fetch("SMTP_PASSWORD"),
+    port: "587",
+    # port: 1025,
+    user_name: ENV.fetch("SMTP_USERNAME")
 }
